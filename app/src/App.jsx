3db968b9f594cd3fe14d873f4dbcf40ccb50ac69
Yusuf Alnasiri <alnasiriyusuf@gmail.com>
2023-08-08 19:16:10 +0300

    a

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+
+import Card from "./assets/Card"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Card />
       
     </>
   )
