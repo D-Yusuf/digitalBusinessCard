13852c490568142068220765e8959a3db6a9960b
Yusuf Alnasiri <alnasiriyusuf@gmail.com>
2023-08-09 20:09:34 +0300

    a

--- app/src/assets/footer.jsx
+++ app/src/assets/footer.jsx
@@ -1,3 +1,15 @@
-export default function Footer(){
-    
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareXTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faSquareFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faSquareInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+export default function Footer() {
+  return (
+    <footer className="bg-[#161619] text-3xl text-[#918E9B] w-full flex items-center justify-around py-[19.5px] px-[48.5px]">
+      <FontAwesomeIcon icon={faSquareXTwitter} />
+      <FontAwesomeIcon icon={faSquareFacebook} />
+      <FontAwesomeIcon icon={faSquareInstagram} />
+      <FontAwesomeIcon icon={faSquareGithub} />
+    </footer>
+  );
 }
